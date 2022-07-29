6a8da01348007b2069b6e6899fd9447693e4e18d
Jayaram s kumar <79137379+Jayaram-s-kumar@users.noreply.github.com>
2022-07-29 21:15:45 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { text } from 'express';
+import React,{useState} from 'react';
+import './App.css';
+
+function App() {
+  const [toDos,setToDos]=useState([])
+  const [toDo,setToDo]=useState("")
+  return (
+    <div className="app">
+      
+    </div>
+  );
+}
+
+export default App;
